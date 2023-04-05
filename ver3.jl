6178dff1d4494cc7e6e24b4d878a4d421e36dbe3
Peikii <43866326+Peikii@users.noreply.github.com>
2023-04-05 19:49:54 -0400

    Add files via upload

--- ver3.jl
+++ ver3.jl
@@ -15,7 +15,7 @@ function main()
 
     # Open the file and memory-map its contents
     f = open(filename)
-    buffer = Mmap.mmap(f, UInt32, file_size)
+    buffer = Mmap.mmap(f, Vector{UInt8}, file_size)
 
     # Initialize thread-local arrays to count the frequency of each character
     count_local = [zeros(Int, 4) for i in 1:nprocs]
